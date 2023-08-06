4701c128b03bd56120d6cb97eba4eee520106247
Yuriy <berlandspw@gmail.com>
2023-08-06 17:07:28 +0500

    footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
+  return (
+    <div>
+      <div className="footerHeading">
+        <h4>PIEX - option creation platform</h4>
+      </div>
+      <div className="socialBlock">
+        <div className="socialItem">Linkedin</div>
+        <div className="socialItem">Telegram</div>
+        <div className="socialItem">Github</div>
+        <div className="socialItem naming">PIEX.tech</div>
+      </div>
+    </div>
+  );
+};
 
-
-    return(
-        <div>
-            Footer
-        </div>
-    )
-}
-
-export default Footer
+export default Footer;
